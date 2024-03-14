6df745fc6e0f08831ab88287c939af5322c12d9a
MatiasX <waldorfosmatyi@gmail.com>
2024-03-14 01:42:25 +0100

    Ingredients bővítve

--- DrinkIsland/src/app/app.component.ts
+++ DrinkIsland/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'DrinkIsland';
+  backgroundImageUrl: string = '/assets/img/iw.jpg';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
--- DrinkIsland/src/app/ingredients/ingredients.component.ts
+++ DrinkIsland/src/app/ingredients/ingredients.component.ts
@@ -7,6 +7,11 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./ingredients.component.css']
 })
 export class IngredientsComponent implements OnInit {
+backgroundImageUrl: string = '/assets/img/iw.jpg';
+items: any;
+toggleAccordion(_t4: any) {
+throw new Error('Method not implemented.');
+}
   ingredients: any[] = [];
 
   constructor(private http: HttpClient) { }
