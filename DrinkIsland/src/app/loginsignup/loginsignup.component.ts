de1ce43f8faf794529bbe7896379acfc10657b06
Sulan Ádám <sulan.adam19@gmail.com>
2024-04-15 10:47:48 +0200

    login,register megcsinálva

--- DrinkIsland/src/app/loginsignup/loginsignup.component.ts
+++ DrinkIsland/src/app/loginsignup/loginsignup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-loginsignup',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./loginsignup.component.css']
 })
 export class LoginsignupComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private loginService: LoginService) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const loginData = {
+        username: this.loginForm.value.username,
+        password: this.loginForm.value.password
+      };
+
+      this.loginService.login(loginData).subscribe(
+        response => {
+          // Handle success response from backend
+          console.log('Login successful:', response);
+        },
+        error => {
+          // Handle error response from backend
+          console.error('Login failed:', error);
+        }
+      );
+    }
+  }
 }
