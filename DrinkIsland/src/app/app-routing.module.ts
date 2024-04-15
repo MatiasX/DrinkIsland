34e615602022ab080a9041fefcded1ed13893a4c
MatiasX <waldorfosmatyi@gmail.com>
2024-04-15 13:36:54 +0200

    Admin elérési útvonalak hozzáadva

--- DrinkIsland/src/app/app-routing.module.ts
+++ DrinkIsland/src/app/app-routing.module.ts
@@ -7,11 +7,15 @@ import { HomeComponent } from './home/home.component';
 import { AlcoholicDrinksComponent } from './recipes/alcoholic-drinks/alcoholic-drinks.component';
 import { NonAlcoholicDrinksComponent } from './recipes/non-alcoholic-drinks/non-alcoholic-drinks.component';
 import { DetailedSearchComponent } from './recipes/detailed-search/detailed-search.component';
-import { IngredientsComponent } from './ingredients/ingredients.component';
+import { IngredientsComponent } from './coctailblog/ingredients.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { LoginsignupComponent } from './loginsignup/loginsignup.component';
 import { JustsignupComponent } from './justsignup/justsignup.component';
 import { SupportComponent } from './support/support.component';
+import { AlapanyagComponent } from './admin/alapanyag/alapanyag.component';
+import { ItalComponent } from './admin/ital/ital.component';
+import { ReceptComponent } from './admin/recept/recept.component';
+import { UserComponent } from './admin/user/user.component';
 
 
 const routes: Routes = [
@@ -25,6 +29,10 @@ const routes: Routes = [
   { path: 'loginsignup', component: LoginsignupComponent },
   { path: 'justsignup', component: JustsignupComponent },
   { path: 'support', component: SupportComponent },
+  { path: 'alapanyag', component: AlapanyagComponent },
+  { path: 'ital', component: ItalComponent },
+  { path: 'recept', component: ReceptComponent },
+  { path: 'user', component: UserComponent },
 ];
 
 @NgModule({
