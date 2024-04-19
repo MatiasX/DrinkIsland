ff62a2aa6e54428bf3cd4f841d63400763d39e9a
Sulan Ádám <sulan.adam19@gmail.com>
2024-04-19 09:46:25 +0200

    véralkoholszint számláló fejlesztve

--- DrinkIsland/src/app/calculator/calculator.service.ts
+++ DrinkIsland/src/app/calculator/calculator.service.ts
@@ -5,11 +5,22 @@ import { Injectable } from '@angular/core';
 })
 export class CalculatorService {
   calculateTimeToSober(weight: number, gender: string, alcoholAmount: number, drinkAlcoholPercentage: number, timeSinceLastDrink: number): number {
-    const soberingRate = gender === 'male' ? 0.015 : 0.017;
-    const gramsOfAlcohol = alcoholAmount * drinkAlcoholPercentage * 10; // Átváltás dl-ből grammra (1 dl = 10 g alkohol)
-    const bloodAlcoholContent = gramsOfAlcohol / (weight * soberingRate);
-    const soberingTime = bloodAlcoholContent * 0.015;
+    const soberingRate = gender === 'male' ? 0.148  : 0.156;
+    const distribution = gender === 'male' ? 0.71  : 0.58;
+    const gramsOfAlcohol = alcoholAmount * drinkAlcoholPercentage * 1.055; // Átváltás ml-ből grammra (1 ml = 1.055 g alkohol)
+    const volumeOfDistribution = weight * distribution;
+    const bloodAlcoholContent = gramsOfAlcohol / volumeOfDistribution - soberingRate * timeSinceLastDrink;
+    const soberingTime = bloodAlcoholContent/soberingRate;
 
     return soberingTime;
+  }
+  calculateBAC(weight: number, gender: string, alcoholAmount: number, drinkAlcoholPercentage: number, timeSinceLastDrink: number): number {
+    const soberingRate = gender === 'male' ? 0.148  : 0.156;
+    const distribution = gender === 'male' ? 0.71  : 0.58;
+    const gramsOfAlcohol = alcoholAmount * drinkAlcoholPercentage * 1.055; // Átváltás ml-ből grammra (1 ml = 1.055 g alkohol)
+    const volumeOfDistribution = weight * distribution;
+    const bloodAlcoholContent = gramsOfAlcohol / volumeOfDistribution - soberingRate * timeSinceLastDrink;
+
+    return bloodAlcoholContent;
   }
 }
