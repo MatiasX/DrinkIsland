63a5277990277b171bbed47ab49baf6a4bb19d72
MatiasX <waldorfosmatyi@gmail.com>
2024-04-25 15:43:36 +0200

    Javítások és dokumentáció hozzáadva.

--- DrinkIsland/src/app/calculator/calculator.component.ts
+++ DrinkIsland/src/app/calculator/calculator.component.ts
@@ -57,32 +57,4 @@ export class CalculatorComponent {
       this.timeSinceLastDrink
     );
   }
-
-
-  // weight: number = 0;
-  // gender: string = '';
-  // alcoholAmount: number = 0;
-  // drinkAlcoholPercentage: number = 0;
-  // timeSinceLastDrink: number = 0;
-  // soberingTime:number = 0;
-  // bloodAlcoholContent: number = 0;
-
-  // constructor(private calculatorService: CalculatorService) {}
-
-  // calculate() {
-  //   this.soberingTime = this.calculatorService.calculateTimeToSober(
-  //     this.weight,
-  //     this.gender,
-  //     this.alcoholAmount,
-  //     this.drinkAlcoholPercentage,
-  //     this.timeSinceLastDrink
-  //   );
-  //   this.bloodAlcoholContent = this.calculatorService.calculateBAC(
-  //     this.weight,
-  //     this.gender,
-  //     this.alcoholAmount,
-  //     this.drinkAlcoholPercentage,
-  //     this.timeSinceLastDrink
-  //   );
-  // }
 }
